8374c53bd9d743f729f9236ab72fa71f644ba174
Wonhyuk Cho <wcho2@nait.ca>
2024-06-16 14:25:34 -0600

    added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isNavbarCollapsed = true;
 
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
 }
